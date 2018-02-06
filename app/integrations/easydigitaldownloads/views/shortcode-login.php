3f487ef2ba3acf06343166fe9b2752ae3b4813fe
Spencer Finnell <spencerfinnell@gmail.com>
2018-02-06 18:04:38 -0500

    Style login form

--- app/integrations/easydigitaldownloads/views/shortcode-login.php
+++ app/integrations/easydigitaldownloads/views/shortcode-login.php
@@ -26,37 +26,38 @@ if ( ! is_user_logged_in() ) :
 
 <div class="container">
 	<div class="row justify-content-center">
-		<div class="col-md-5">
+		<div class="col-md-6">
 
 			<?php edd_print_errors(); ?>
 
-			<form id="edd_login_form" action="" method="post">
+			<form id="edd_login_form" class="card card__inner" action="" method="post">
 
-				<p class="edd-login-username">
-					<label for="edd_user_login"><?php esc_html_e( 'Username or Email', 'bigbox' ); ?></label>
-					<input name="edd_user_login" id="edd_user_login" class="edd-required edd-input" type="text"/>
+				<p class="form-row">
+					<label for="edd_user_login" class="form-label"><?php esc_html_e( 'Username or Email', 'bigbox' ); ?></label>
+					<input name="edd_user_login" id="edd_user_login" class="form-input" type="text"/>
 				</p>
 
-				<p class="edd-login-password">
-					<label for="edd_user_pass"><?php esc_html_e( 'Password', 'bigbox' ); ?></label>
-					<input name="edd_user_pass" id="edd_user_pass" class="edd-password edd-required edd-input" type="password"/>
+				<p class="form-row">
+					<label for="edd_user_pass" class="form-label"><?php esc_html_e( 'Password', 'bigbox' ); ?></label>
+					<input name="edd_user_pass" id="edd_user_pass" class="form-input" type="password"/>
 				</p>
 
-				<p class="edd-login-remember">
-					<label><input name="rememberme" type="checkbox" id="rememberme" value="forever" /> 
-					<?php esc_html_e( 'Remember Me', 'bigbox' ); ?></label>
-				</p>
-
-				<p class="edd-login-submit">
+				<p class="form-row form-row--submit">
 					<input type="hidden" name="edd_redirect" value="<?php echo esc_url( $edd_login_redirect ); ?>"/>
-					<input type="hidden" name="edd_login_nonce" value="<?php echo wp_create_nonce( 'edd-login-nonce' ); ?>"/>
+					<input type="hidden" name="edd_login_nonce" value="<?php echo esc_attr( wp_create_nonce( 'edd-login-nonce' ) ); ?>"/>
 					<input type="hidden" name="edd_action" value="user_login"/>
-					<input id="edd_login_submit" type="submit" class="edd-submit" value="<?php _e( 'Log In', 'bigbox' ); ?>"/>
+					<input id="edd_login_submit" type="submit" class="edd-submit" value="<?php esc_attr_e( 'Sign In', 'bigbox' ); ?>"/>
 				</p>
 
-				<p class="edd-lost-password">
-					<a href="<?php echo wp_lostpassword_url(); ?>">
-						<?php _e( 'Lost Password?', 'bigbox' ); ?>
+				<p class="form-row form-row--actions">
+
+					<label class="form-label form-label--checkbox">
+						<input name="rememberme" type="checkbox" id="rememberme" value="forever" class="form-input form-input--checkbox" /> 
+						<span><?php esc_html_e( 'Remember Me', 'bigbox' ); ?></span>
+					</label>
+
+					<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>">
+						<?php esc_attr_e( 'Lost Password?', 'bigbox' ); ?>
 					</a>
 				</p>
 
@@ -67,4 +68,5 @@ if ( ! is_user_logged_in() ) :
 	</div>
 </div>
 
-<?php endif; ?>
+<?php
+endif;
