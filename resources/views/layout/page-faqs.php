d9a6b918c362c2e6855ddd37b34a6b60d2085df5
Spencer Finnell <spencerfinnell@gmail.com>
2018-05-24 08:55:46 -0400

    FAQs landing page.

---
+++ resources/views/layout/page-faqs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: FAQs
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ * @package BigBox
+ * @category Theme
+ * @author Spencer Finnell
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+bigbox_view( 'global/header' );
+
+while ( have_posts() ) :
+	the_post();
+
+	bigbox_partial( 'features-hero' );
+	bigbox_partial( 'features-faqs', [
+		'alt' => false,
+	]	);
+	bigbox_partial( 'cta-buy' );
+endwhile;
+
+bigbox_view( 'global/footer' );
