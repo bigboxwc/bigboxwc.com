bd74af0f6c62d4c73305403297d688fee6ccd57d
Spencer Finnell <spencerfinnell@gmail.com>
2018-06-28 12:00:08 -0400

    Add demos menu item.

--- resources/views/partials/access-guest.php
+++ resources/views/partials/access-guest.php
@@ -17,6 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <nav id="access" class="access">
 	<a href="#access-toggle" class="access-item--close access-item" aria-label="Close">&times; Close</a>
 	<a href="/features/" class="access-item <?php echo esc_attr( is_page( 'features' ) ? 'access-item--active' : null ); ?>">All Features</a>
+	<a href="/theme-demos/" class="access-item <?php echo esc_attr( is_page( 'theme-demos' ) ? 'access-item--active' : null ); ?>">Live Demo</a>
 	<a href="https://blog.bigboxwc.com/" class="access-item">Blog &amp; Resources</a>
 	<a href="/account/" class="access-item">Account</a>
 
