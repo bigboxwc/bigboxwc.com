ecc88fa726840071056abd9986998ae3fe48b06b
Spencer Finnell <spencerfinnell@gmail.com>
2018-06-28 11:56:20 -0400

    Update URL.

--- resources/views/partials/features-overview.php
+++ resources/views/partials/features-overview.php
@@ -66,6 +66,6 @@ global $post;
 			<a href="/buy/" class="button button--primary">Get BigBox Now</a> 
 		<?php endif; ?>
 
-		<a href="/demos/" class="block-cta__sublink">View Demo &rarr;</a>
+		<a href="/theme-demos/" class="block-cta__sublink">View Demo &rarr;</a>
 	</div>
 </div>
