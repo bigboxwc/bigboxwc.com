9cf1b7478d92c81cbca51696fe81295b4e3ff483
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-11 09:51:01 -0400

    Remove featured image from blog.

--- resources/views/partials/blog/content.php
+++ resources/views/partials/blog/content.php
@@ -16,11 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="blog-post">
-	<div class="<?php echo esc_attr( ! is_page() ? 'alignwide' : 'alignnone' ); ?>">
-		<a href="<?php the_permalink(); ?>">
-			<?php the_post_thumbnail( ! is_page() ? 'large' : 'medium' ); ?>
-		</a>
-	</div>
 
 	<div class="block-header blog-post__header">
 		<h3 class="block-title blog-post__title">
